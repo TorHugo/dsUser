25a18bc2c21e9e622df9e245c27fc69dba0885b7
TorHugo <victor.jesus@confitec.com.br>
2022-12-13 23:11:35 -0300

    Primeiro commit: - Subindo projeto para o Git, para um melhor versionamento.

---
+++ dsusers/src/main/java/com/torhugo/dsusers/DsusersApplication.java
@@ -0,0 +1,13 @@
+package com.torhugo.dsusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsusersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsusersApplication.class, args);
+	}
+
+}
---
+++ dsusers/src/test/java/com/torhugo/dsusers/DsusersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torhugo.dsusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsusersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
